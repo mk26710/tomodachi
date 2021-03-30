4a6f6c776fc7ea20e8ef95cf4bf517261cb38f34
howaitoreivun <72914371+howaitoreivun@users.noreply.github.com>
2021-03-30 17:24:15 +0400

    Created time units converter
    will be used for reminders and maybe for other time based inputs ¯\_(ツ)_/¯

--- tomodachi/utils/converters.py
+++ tomodachi/utils/converters.py
@@ -4,6 +4,9 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import re
+from datetime import timedelta
+
 from discord.ext.commands import Converter, BadArgument
 
 
@@ -13,3 +16,25 @@ class uint(Converter, int):  # noqa
             raise BadArgument(f"{argument} must be an unsigned integer")
 
         return int(argument)
+
+
+time_regex = re.compile(r"(\d{1,5})([smhd])")
+
+units_to_seconds = {
+    "d": 86400,
+    "h": 3600,
+    "m": 60,
+    "s": 1,
+}
+
+
+class TimeUnit(Converter, timedelta):
+    async def convert(self, ctx, argument):
+        match = re.match(time_regex, argument)
+        if not match:
+            raise BadArgument(f'"{argument}" is invalid time input.')
+
+        amount, unit = match.groups()
+        seconds = units_to_seconds.get(unit) * int(amount)
+
+        return timedelta(seconds=seconds)
