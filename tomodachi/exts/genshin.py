8b4a4f8c0394326f10754d1016ca8bd08b1e6c1a
httport <96192210+httport@users.noreply.github.com>
2021-03-31 10:35:17 +0400

    now resin check command can create reminders for users :O

--- tomodachi/exts/genshin.py
+++ tomodachi/exts/genshin.py
@@ -6,9 +6,11 @@
 
 import asyncio
 from datetime import timedelta
+from typing import Optional
 
 import humanize
 from discord.ext import commands
+from jishaku.models import copy_context_with
 
 from tomodachi.core import Tomodachi, TomodachiContext
 from tomodachi.utils.converters import uint
@@ -20,14 +22,42 @@ class Genshin(commands.Cog):
 
     @commands.cooldown(1, 5.0, commands.BucketType.user)
     @commands.command(help="Counts how much time left until your resin refills")
-    async def resin(self, ctx: TomodachiContext, current: uint = 0, needed: uint = 160):
+    async def resin(
+        self,
+        ctx: TomodachiContext,
+        current: Optional[uint] = 0,
+        needed: Optional[uint] = 160,
+        remind: Optional[bool] = True,
+    ):
         if current < 0 or current > 160 or needed < current:
             return await ctx.send("You either reached the cap or trying to put invalid amount.")
 
         to_wait = timedelta(minutes=((needed - current) * 8))
         h_delta = await asyncio.to_thread(humanize.precisedelta, to_wait)
 
-        await ctx.send(f"You need to wait {h_delta} until you reach `{needed}` resin")
+        await ctx.send(f"You will have {needed} resin **in {h_delta}**.")
+
+        if not remind:
+            return
+
+        emoji = ctx.icon("slowmode")
+
+        msg = await ctx.send(f"React with {emoji} to create a reminder!")
+        await msg.add_reaction(emoji)
+
+        def check(reaction, user):
+            return user.id == ctx.author.id and reaction.emoji.id == emoji.id and reaction.message.id == msg.id
+
+        try:
+            await self.bot.wait_for("reaction_add", timeout=30.0, check=check)
+        except asyncio.TimeoutError:
+            pass
+        else:
+            command_string = f"{ctx.prefix}reminder add {to_wait.seconds}s Genshin Impact: {needed} resin refilled!"
+            alt_ctx = await copy_context_with(ctx, author=ctx.author, content=command_string)
+            await alt_ctx.command.invoke(alt_ctx)
+        finally:
+            await msg.delete()
 
 
 def setup(bot):
