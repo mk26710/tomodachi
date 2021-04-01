a532b0d34f1008d145f4743aee4b389e7701d4ed
howaitoreivun <72914371+howaitoreivun@users.noreply.github.com>
2021-04-01 16:41:12 +0400

    Created custom database backend so we can have asyncio.Event and pool easier to access

--- tomodachi/utils/database/instance.py
+++ tomodachi/utils/database/instance.py
@@ -4,9 +4,35 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from databases import Database
+import asyncio
+import typing
+
+from databases import Database, DatabaseURL
+
 from config import POSTGRES_DSN
 
 __all__ = ["db"]
 
-db = Database(POSTGRES_DSN)
+
+class TomodachiDatabase(Database):
+    SUPPORTED_BACKENDS = {
+        "postgresql": "databases.backends.postgres:PostgresBackend",
+    }
+
+    def __init__(self, url: typing.Union[str, "DatabaseURL"], **options: typing.Any):
+        super().__init__(url, **options)
+        self._connection_established = asyncio.Event()
+
+    @property
+    def pool(self):
+        return self._backend._pool  # noqa
+
+    async def wait_until_connected(self):
+        await self._connection_established.wait()
+
+    async def connect(self) -> None:
+        await super(TomodachiDatabase, self).connect()
+        self._connection_established.set()
+
+
+db = TomodachiDatabase(POSTGRES_DSN)
