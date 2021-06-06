b3c5c57ceca8771f91971164217c8a634a1361b6
howaitoreivun <72914371+howaitoreivun@users.noreply.github.com>
2021-06-06 21:53:32 +0400

    Optimized imports

--- tomodachi/exts/genshin.py
+++ tomodachi/exts/genshin.py
@@ -5,8 +5,8 @@
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import asyncio
-from typing import Optional
 from datetime import timedelta
+from typing import Optional
 
 import humanize
 from discord.ext import commands
--- tomodachi/utils/database/__init__.py
+++ tomodachi/utils/database/__init__.py
@@ -4,5 +4,5 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from .schema import *
 from .instance import *
+from .schema import *
