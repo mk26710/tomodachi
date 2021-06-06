786d08cd1b665aa764185db57e1c9a9e85a9ddad
httport <96192210+httport@users.noreply.github.com>
2021-06-06 16:44:59 +0400

    another helper function but for avatars

--- tomodachi/utils/helpers.py
+++ tomodachi/utils/helpers.py
@@ -5,7 +5,7 @@ import discord
 from tomodachi.core.icons import Icons
 from tomodachi.utils import HUMAN_READABLE_FLAGS
 
-__all__ = ["humanize_flags"]
+__all__ = ["humanize_flags", "avatar_or_default"]
 
 
 def _humanize_iteration_filter(o: Tuple[str, bool]):
@@ -15,3 +15,9 @@ def _humanize_iteration_filter(o: Tuple[str, bool]):
 def humanize_flags(flags: discord.PublicUserFlags):
     for name, value in filter(_humanize_iteration_filter, flags):
         yield f"{Icons()(name)} {HUMAN_READABLE_FLAGS[name]}"
+
+
+def avatar_or_default(user: discord.User):
+    if not user.avatar:
+        return user.default_avatar
+    return user.avatar
