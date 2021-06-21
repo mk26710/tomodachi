e8adb85c4c3d869f45a9c699aa4e44d5ea8ac568
httport <96192210+httport@users.noreply.github.com>
2021-06-21 11:14:16 +0400

    Ok you know what? No views. Reactions forever.

--- tomodachi/exts/genshin.py
+++ tomodachi/exts/genshin.py
@@ -6,63 +6,21 @@
 
 from __future__ import annotations
 
-from typing import Callable, Optional, Coroutine
+import asyncio
+from typing import Optional
 from datetime import datetime, timedelta
 
 import discord
 from discord.ext import commands
+from jishaku.models import copy_context_with
 
 from tomodachi.core import CogMixin, TomodachiContext
 from tomodachi.utils import timestamp
-from tomodachi.exts.reminders import Reminder
+from tomodachi.utils.icons import i
 from tomodachi.utils.converters import uint
 
 
-class CreateReminderView(discord.ui.View):
-    def __init__(self, callback: Callable[[discord.Interaction], Coroutine], *, timeout=60.0):
-        self.callback = callback
-
-        super().__init__(timeout=timeout)
-
-    @discord.ui.button(label="Remind me about that")
-    async def btn(self, _, interaction: discord.Interaction):
-        await self.callback(interaction, self.stop)
-
-
 class Genshin(CogMixin, icon=discord.PartialEmoji(name="cryo", id=853553127541702679)):
-    def reminder_callback(self, message: discord.Message, delta: timedelta, amount: int):
-        async def callback(interaction: discord.Interaction, stop: Callable):
-            if interaction.user.id != message.author.id:
-                return await interaction.response.send("You can't interact with others buttons!", ephemeral=True)
-
-            now = datetime.utcnow()
-            trigger_at = now + delta
-
-            reminder = Reminder(
-                created_at=now,
-                trigger_at=trigger_at,
-                author_id=message.author.id,
-                guild_id=message.guild.id,
-                channel_id=message.channel.id,
-                message_id=message.id,
-                contents=f"Genshin Impact: you now have {amount} resin!",
-            )
-
-            reminder = await self.bot.get_cog("Reminders").create_reminder(reminder)
-
-            identifier = ""
-            if reminder.id:
-                identifier = f" (#{reminder.id})"
-
-            when = timestamp(trigger_at)
-
-            await interaction.response.send_message(
-                f":ok_hand: I will remind you about this on {when}{identifier}", ephemeral=True
-            )
-            stop()
-
-        return callback
-
     @commands.cooldown(1, 5.0, commands.BucketType.user)
     @commands.command(help="Counts how much time left until your resin refills")
     async def resin(
@@ -70,7 +28,7 @@ class Genshin(CogMixin, icon=discord.PartialEmoji(name="cryo", id=85355312754170
         ctx: TomodachiContext,
         current: Optional[uint] = 0,
         needed: Optional[uint] = 160,
-        remind: Optional[bool] = True,
+        remind: Optional[bool] = False,
     ):
         if current < 0 or current > 160 or needed < current:
             return await ctx.send("You either reached the cap or trying to put invalid amount.")
@@ -78,11 +36,26 @@ class Genshin(CogMixin, icon=discord.PartialEmoji(name="cryo", id=85355312754170
         delta = timedelta(minutes=((needed - current) * 8))
         humanized = timestamp(datetime.now() + delta)
 
-        if not remind:
-            await ctx.send(f"You will have {needed} resin **{humanized:R}**.")
+        to_send = f"You will have **{needed}** resin at {humanized:F}."
+        if remind:
+            to_send += f"\nReact with {i:slowmode} to create a reminder."
         else:
-            view = CreateReminderView(self.reminder_callback(ctx.message, delta, needed))
-            await ctx.send(f"You will have {needed} resin **{humanized:R}**.", view=view)
+            return await ctx.send(to_send)
+
+        msg = await ctx.send(to_send)
+        await msg.add_reaction(i("slowmode"))
+
+        def check(r, user):
+            return user.id == ctx.author.id and r.emoji.id == i("slowmode").id
+
+        try:
+            await self.bot.wait_for("reaction_add", timeout=30.0, check=check)
+        except asyncio.TimeoutError:
+            pass
+        else:
+            command_string = f"{ctx.prefix}reminder add {delta.seconds}s Genshin Impact: {needed} resin refilled!"
+            alt_ctx = await copy_context_with(ctx, author=ctx.author, content=command_string)
+            await alt_ctx.command.invoke(alt_ctx)
 
 
 def setup(bot):
