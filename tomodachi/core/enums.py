4788feaa0c200d8219b1d8bb9fdf0c51c3dba7ba
howaitoreivun <72914371+howaitoreivun@users.noreply.github.com>
2021-06-24 15:57:53 +0400

    Enum for actions

---
+++ tomodachi/core/enums.py
@@ -0,0 +1,15 @@
+#  Copyright (c) 2020 — present, howaitoreivun.
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from enum import Enum
+
+__all__ = ["ActionType"]
+
+
+class ActionType(Enum):
+    REMINDER = "REMINDER"
+    MODERATION = "MODERATION"
+    NOTIFICATION = "NOTIFICATION"
