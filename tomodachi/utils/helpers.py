a0105056cc89c96c2afafcabc8cce166171771af
httport <96192210+httport@users.noreply.github.com>
2021-06-29 16:24:52 +0400

    Missing license headers

--- tomodachi/utils/helpers.py
+++ tomodachi/utils/helpers.py
@@ -1,3 +1,9 @@
+#  Copyright (c) 2020 — present, howaitoreivun.
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from typing import Tuple
 from datetime import datetime, timezone
 from collections import defaultdict
