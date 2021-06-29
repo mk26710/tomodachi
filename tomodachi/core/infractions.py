4fa0e0c0f23b81eeb0b84f91b0762c8268dbd6ef
howaitoreivun <72914371+howaitoreivun@users.noreply.github.com>
2021-06-29 16:14:02 +0400

    Changed Infraction.create method
    Now it accepts same keyword arguments as Infraction.__init__ basically

--- tomodachi/core/infractions.py
+++ tomodachi/core/infractions.py
@@ -4,6 +4,8 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+#  This file is absolute mess, don't read unless you enjoy tormenting yourslef :)
+
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any, Optional
@@ -53,7 +55,28 @@ class Infractions:
             return None
         return Infraction(**record)
 
-    async def create(self, infraction: Infraction, *, permanent=False):
+    async def create(
+        self,
+        infraction: Optional[Infraction] = None,
+        *,
+        inf_type: Optional[InfractionType] = None,
+        expires_at: Optional[datetime] = None,
+        guild_id: Optional[int] = None,
+        mod_id: Optional[int] = None,
+        target_id: Optional[int] = None,
+        reason: Optional[str] = None,
+        create_action: bool = True,
+    ):
+        """Creates an infraction and store it in database"""
+        # if user doesn't provide an infraction object the function will create it
+        infraction = infraction or Infraction(
+            inf_type=inf_type,
+            expires_at=expires_at,
+            guild_id=guild_id,
+            mod_id=mod_id,
+            target_id=target_id,
+            reason=reason,
+        )
         # action_type, trigger_at, author_id, guild_id, channel_id, message_id, extra - are required fields
         # but since it's an infraction we can ignore channel and message ids because it is not a reminder
         action = Action(
@@ -62,20 +85,20 @@ class Infractions:
             guild_id=infraction.guild_id,
         )
 
-        if not permanent:
+        if create_action:
             # override prepared action object with the one from db
             action = await self.bot.actions.create_action(action)
 
         async with self.bot.db.pool.acquire() as conn:
-            query = """INSERT INTO infractions 
+            query = """INSERT INTO infractions
             (action_id, inf_type, created_at, expires_at, guild_id, mod_id, target_id, reason)
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
             RETURNING *;"""
 
             record = await conn.fetchrow(
                 query,
                 action.id,
-                infraction.inf_type.name,
+                getattr(infraction.inf_type, "name", None),
                 action.created_at,
                 infraction.expires_at,
                 infraction.guild_id,
