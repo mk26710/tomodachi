d18b96f042b9a89c67714f74dee785c146484cf2
howaitoreivun <72914371+howaitoreivun@users.noreply.github.com>
2021-07-09 12:58:28 +0400

    Class that makes booleans humanized because I'm lazy

---
+++ tomodachi/utils/humanbool.py
@@ -0,0 +1,17 @@
+#  Copyright (c) 2020 — present, snezhniy.
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+class humanbool:
+    def __init__(self, value: bool):
+        if not isinstance(value, bool):
+            raise TypeError("humanbool values can be only booleans.")
+        self.value = value
+
+    def __str__(self):
+        if self.value is True:
+            return "enabled"
+        else:
+            return "disabled"
